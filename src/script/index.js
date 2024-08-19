2547b00a5b62fe8569b5ab2c7962820f4c96854f
shidiqi-coding <shidiqiihsan@gmail.com>
2024-08-19 15:13:32 +0700

    Create index.js

---
+++ src/script/index.js
@@ -0,0 +1,10 @@
+import "./archive-items.js";
+import "./archived-list.js";
+
+import "./input-form.js";
+
+import "./notes-item.js";
+import "./note-list.js";
+
+import "./logo-bar.js";
+import "./my-footer.js";
